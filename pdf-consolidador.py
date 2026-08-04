ea8dfca293b27abda0be7b24911f14d0cd04dd4c
Sérgio Ricardo Vieira Torres Silva <working7.sergio@gmail.com>
2026-08-04 09:49:04 -0300

    tipo: criação automática de pastas

---
+++ pdf-consolidador.py
@@ -0,0 +1,11 @@
+import PyPDF2
+import os
+
+#criar uma parsta de entrada
+if not os.path.exists("entrada"):
+    os.makedirs("entrada")
+
+#criar uma pasta de saída
+if not os.path.exists("saida"):
+    os.makedirs("saida")
+
